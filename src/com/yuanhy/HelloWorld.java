08a658e2ecdc09e26dad16683071c3379c1dd150
yuanhy <yuanhy@keyou.cn>
2019-03-20 21:46:44 +0800

    Initial commit

---
+++ src/com/yuanhy/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.yuanhy;
+
+import java.util.ArrayList;
+
+public class HelloWorld {
+    public static  void main(String [] args){
+        int age =10;
+        System.out.println("age = " + age);
+        System.out.println("HelloWorld!");
+
+
+
+    }
+}
